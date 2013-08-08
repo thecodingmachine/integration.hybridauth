5095c480b6f99a34aa35278d4b9aa715f408c7a4
David Négrier <d.negrier@thecodingmachine.com>
2013-08-08 14:23:34 +0200

    Developing install process

--- src/Mouf/Integration/HybridAuth/Controllers/HybridAuthInstallController.php
+++ src/Mouf/Integration/HybridAuth/Controllers/HybridAuthInstallController.php
@@ -53,7 +53,7 @@ class HybridAuthInstallController extends Controller {
 			$this->moufManager = MoufManager::getMoufManagerHiddenInstance();
 		}
 				
-		$this->content->addFile(dirname(__FILE__)."/../../../views/installStep1.php", $this);
+		$this->content->addFile(dirname(__FILE__)."/../../../../views/installStep1.php", $this);
 		$this->template->toHtml();
 	}
 
@@ -91,7 +91,7 @@ class HybridAuthInstallController extends Controller {
 			return;
 		}
 								
-		$this->content->addFile(dirname(__FILE__)."/../../../views/installStep2.php", $this);
+		$this->content->addFile(dirname(__FILE__)."/../../../../views/installStep2.php", $this);
 		$this->template->toHtml();
 	}
 	
@@ -102,7 +102,10 @@ class HybridAuthInstallController extends Controller {
 	 * @param string $name
 	 * @param bool $selfedit
 	 */
-	public function generate($sourcedirectory, $daonamespace, $beannamespace, $keepSupport = 0, $selfedit="false") {
+	public function generate($facebook = "", $facebook_id = "", $facebook_secret = "", $facebook_scope = "", 
+			$google = "", $google_id = "", $google_secret = "",
+			$twitter = "", $twitter_key = "", $twitter_secret = "",
+			$selfedit="false") {
 		$this->selfedit = $selfedit;
 		
 		if ($selfedit == "true") {
@@ -112,11 +115,233 @@ class HybridAuthInstallController extends Controller {
 		}
 		
 		
+		$moufManager = $this->moufManager;
+		
+		$configManager = $moufManager->getConfigManager();
+		$constants = $configManager->getMergedConstants();
+		
+		$configPhpConstants = $configManager->getDefinedConstants();
+		
+		$providers = array();
+		
+		if ($facebook) {
+			if (!isset($constants['FACEBOOK_ID'])) {
+				$configManager->registerConstant('FACEBOOK_ID', 'string', '', 'The Facebook ID used by Facebook API. You can create an app here: https://developers.facebook.com/apps');
+			}
+			if (!isset($constants['FACEBOOK_SECRET'])) {
+				$configManager->registerConstant('FACEBOOK_SECRET', 'string', '', 'The Facebook secret used by Facebook API. You can create an app here: https://developers.facebook.com/apps');
+			}
+			if (!isset($constants['FACEBOOK_SCOPE'])) {
+				$configManager->registerConstant('FACEBOOK_SCOPE', 'string', '', 'The Facebook scope requested by the application.
+	For instance: "email, user_about_me, user_birthday, user_hometown"');
+			}
+
+			$configPhpConstants['FACEBOOK_ID'] = $facebook_id;
+			$configPhpConstants['FACEBOOK_SECRET'] = $facebook_secret;
+			$configPhpConstants['FACEBOOK_SCOPE'] = $facebook_scope;
+				
+			
+			$googleProvider = InstallUtils::getOrCreateInstance('facebookProvider', 'Mouf\\Integration\\HybridAuth\\GenericProvider', $moufManager);
+			
+			if (!$facebookProvider->getConstructorArgumentProperty('setProviderName')->isValueSet()) {
+				$facebookProvider->getSetterProperty('setProviderName')->setValue('Facebook');
+			}
+			if (!$facebookProvider->getConstructorArgumentProperty('setEnabled')->isValueSet()) {
+				$facebookProvider->getSetterProperty('setEnabled')->setValue(true);
+			}
+			if (!$facebookProvider->getConstructorArgumentProperty('setId')->isValueSet()) {
+				$facebookProvider->getSetterProperty('setId')->setValue('FACEBOOK_ID');
+				$facebookProvider->getSetterProperty('setId')->setOrigin("config");
+			}
+			if (!$facebookProvider->getConstructorArgumentProperty('setSecret')->isValueSet()) {
+				$facebookProvider->getSetterProperty('setSecret')->setValue('FACEBOOK_SECRET');
+				$facebookProvider->getSetterProperty('setSecret')->setOrigin("config");
+			}
+			if (!$facebookProvider->getConstructorArgumentProperty('setScope')->isValueSet()) {
+				$facebookProvider->getSetterProperty('setScope')->setValue('FACEBOOK_SCOPE');
+				$facebookProvider->getSetterProperty('setScope')->setOrigin("config");
+			}
+			$providers[] = $facebookProvider;
+		}
+		
+		if ($google) {
+			if (!isset($constants['GOOGLE_ID'])) {
+				$configManager->registerConstant('GOOGLE_ID', 'string', '', 'The Google ID used to by Google API for connection. You can create an app here: https://code.google.com/apis/console/');
+			}
+			if (!isset($constants['GOOGLE_SECRET'])) {
+				$configManager->registerConstant('GOOGLE_SECRET', 'string', '', 'The Google secret used by Google API.');
+			}
+			
+
+			$configPhpConstants['GOOGLE_ID'] = $google_id;
+			$configPhpConstants['GOOGLE_SECRET'] = $google_secret;
+			
+			$googleProvider = InstallUtils::getOrCreateInstance('googleProvider', 'Mouf\\Integration\\HybridAuth\\GenericProvider', $moufManager);
+			
+			if (!$googleProvider->getConstructorArgumentProperty('setProviderName')->isValueSet()) {
+				$googleProvider->getSetterProperty('setProviderName')->setValue('Google');
+			}
+			if (!$googleProvider->getConstructorArgumentProperty('setEnabled')->isValueSet()) {
+				$googleProvider->getSetterProperty('setEnabled')->setValue(true);
+			}
+			if (!$googleProvider->getConstructorArgumentProperty('setId')->isValueSet()) {
+				$googleProvider->getSetterProperty('setId')->setValue('GOOGLE_ID');
+				$googleProvider->getSetterProperty('setId')->setOrigin("config");
+			}
+			if (!$googleProvider->getConstructorArgumentProperty('setSecret')->isValueSet()) {
+				$googleProvider->getSetterProperty('setSecret')->setValue('GOOGLE_SECRET');
+				$googleProvider->getSetterProperty('setSecret')->setOrigin("config");
+			}
+			
+			$providers[] = $googleProvider;
+		}
+		
+		if ($twitter) {
+			if (!isset($constants['TWITTER_KEY'])) {
+				$configManager->registerConstant('TWITTER_KEY', 'string', '', 'The Twitter key used by Twitter API. You can create an app here: https://dev.twitter.com/apps');
+			}
+			if (!isset($constants['TWITTER_SECRET'])) {
+				$configManager->registerConstant('TWITTER_SECRET', 'string', '', 'The Twitter secret used to by Twitter API');
+			}
+			
+			$configPhpConstants['TWITTER_ID'] = $twitter_id;
+			$configPhpConstants['TWITTER_SECRET'] = $twitter_secret;
+			
+			$twitterProvider = InstallUtils::getOrCreateInstance('twitterProvider', 'Mouf\\Integration\\HybridAuth\\GenericProvider', $moufManager);
+			
+			if (!$twitterProvider->getConstructorArgumentProperty('setProviderName')->isValueSet()) {
+				$twitterProvider->getSetterProperty('setProviderName')->setValue('Twitter');
+			}
+			if (!$twitterProvider->getConstructorArgumentProperty('setEnabled')->isValueSet()) {
+				$twitterProvider->getSetterProperty('setEnabled')->setValue(true);
+			}
+			if (!$twitterProvider->getConstructorArgumentProperty('setKey')->isValueSet()) {
+				$twitterProvider->getSetterProperty('setKey')->setValue('TWITTER_KEY');
+				$twitterProvider->getSetterProperty('setKey')->setOrigin("config");
+			}
+			if (!$twitterProvider->getConstructorArgumentProperty('setSecret')->isValueSet()) {
+				$twitterProvider->getSetterProperty('setSecret')->setValue('TWITTER_SECRET');
+				$twitterProvider->getSetterProperty('setSecret')->setOrigin("config");
+			}
+			
+			$providers[] = $twitterProvider;
+		}
+		
+		$configManager->setDefinedConstants($configPhpConstants);
+		
+		
+		// These instances are expected to exist when the installer is run.
+		$dbConnection = $moufManager->getInstance('dbConnection');
+		$userService = $moufManager->getInstance('userService');
+		$userMessageService = $moufManager->getInstance('userMessageService');
+		
+		// Let's create the instances.
+		$socialAuthenticateUrl = InstallUtils::getOrCreateInstance('socialAuthenticateUrl', 'Mouf\\Mvc\\Splash\\UrlEntryPoint', $moufManager);
+		$socialProviderName = InstallUtils::getOrCreateInstance('socialProviderName', 'Mouf\\Utils\\Value\\Variable', $moufManager);
+		$socialProfile = InstallUtils::getOrCreateInstance('socialProfile', 'Mouf\\Utils\\Value\\Variable', $moufManager);
+		$hybridAuthFactory = InstallUtils::getOrCreateInstance('hybridAuthFactory', 'Mouf\\Integration\\HybridAuth\\HybridAuthFactory', $moufManager);
+		$socialLoginErrorMessage = InstallUtils::getOrCreateInstance('socialLoginErrorMessage', 'Mouf\\Utils\\Value\\Variable', $moufManager);
+		$socialLoginException = InstallUtils::getOrCreateInstance('socialLoginException', 'Mouf\\Utils\\Value\\Variable', $moufManager);
+		$anonymousAssign = $moufManager->createInstance('Mouf\\Utils\\Action\\Assign');
+		$anonymousRequestParam = $moufManager->createInstance('Mouf\\Utils\\Value\\RequestParam');
+		$anonymousSocialAuthenticateAction = $moufManager->createInstance('Mouf\\Integration\\HybridAuth\\Actions\\SocialAuthenticateAction');
+		$anonymousPerformSocialLoginAction = $moufManager->createInstance('Mouf\\Integration\\HybridAuth\\Actions\\PerformSocialLoginAction');
+		$anonymousSelect = $moufManager->createInstance('SQLParser\\Query\\Select');
+		$anonymousColRef = $moufManager->createInstance('SQLParser\\Node\\ColRef');
+		$anonymousTable = $moufManager->createInstance('SQLParser\\Node\\Table');
+		$anonymousAndOp = $moufManager->createInstance('SQLParser\\Node\\AndOp');
+		$anonymousEqual = $moufManager->createInstance('SQLParser\\Node\\Equal');
+		$anonymousColRef2 = $moufManager->createInstance('SQLParser\\Node\\ColRef');
+		$anonymousParameter = $moufManager->createInstance('SQLParser\\Node\\Parameter');
+		$anonymousParamAvailableCondition = $moufManager->createInstance('Mouf\\Database\\QueryWriter\\Condition\\ParamAvailableCondition');
+		$anonymousEqual2 = $moufManager->createInstance('SQLParser\\Node\\Equal');
+		$anonymousColRef3 = $moufManager->createInstance('SQLParser\\Node\\ColRef');
+		$anonymousParameter2 = $moufManager->createInstance('SQLParser\\Node\\Parameter');
+		$anonymousParamAvailableCondition2 = $moufManager->createInstance('Mouf\\Database\\QueryWriter\\Condition\\ParamAvailableCondition');
+		$anonymousSelect2 = $moufManager->createInstance('SQLParser\\Query\\Select');
+		$anonymousColRef4 = $moufManager->createInstance('SQLParser\\Node\\ColRef');
+		$anonymousTable2 = $moufManager->createInstance('SQLParser\\Node\\Table');
+		$anonymousEqual3 = $moufManager->createInstance('SQLParser\\Node\\Equal');
+		$anonymousColRef5 = $moufManager->createInstance('SQLParser\\Node\\ColRef');
+		$anonymousParameter3 = $moufManager->createInstance('SQLParser\\Node\\Parameter');
+		$anonymousParamAvailableCondition3 = $moufManager->createInstance('Mouf\\Database\\QueryWriter\\Condition\\ParamAvailableCondition');
+		$anonymousDisplayMessageAction = $moufManager->createInstance('Mouf\\Html\\Widgets\\MessageService\\Service\\Actions\\DisplayMessageAction');
+		$anonymousUserMessage = $moufManager->createInstance('Mouf\\Html\\Widgets\\MessageService\\Service\\UserMessage');
+		
+		// Let's bind instances together.
+		if (!$socialAuthenticateUrl->getConstructorArgumentProperty('url')->isValueSet()) {
+			$socialAuthenticateUrl->getConstructorArgumentProperty('url')->setValue('authenticate');
+		}
+		if (!$socialAuthenticateUrl->getConstructorArgumentProperty('actions')->isValueSet()) {
+			$socialAuthenticateUrl->getConstructorArgumentProperty('actions')->setValue(array(0 => $anonymousAssign, 1 => $anonymousSocialAuthenticateAction, ));
+		}
+		if (!$hybridAuthFactory->getConstructorArgumentProperty('providers')->isValueSet()) {
+			$hybridAuthFactory->getConstructorArgumentProperty('providers')->setValue($providers);
+		}
+		if (!$hybridAuthFactory->getConstructorArgumentProperty('setDebugMode')->isValueSet()) {
+			$hybridAuthFactory->getSetterProperty('setDebugMode')->setValue(true);
+		}
+		if (!$hybridAuthFactory->getConstructorArgumentProperty('setDebugFile')->isValueSet()) {
+			$hybridAuthFactory->getSetterProperty('setDebugFile')->setValue('hybridAuth.log');
+		}
+		
+		
+		$anonymousAssign->getConstructorArgumentProperty('variable')->setValue($socialProviderName);
+		$anonymousAssign->getConstructorArgumentProperty('value')->setValue($anonymousRequestParam);
+		$anonymousRequestParam->getConstructorArgumentProperty('paramName')->setValue('provider');
+		$anonymousSocialAuthenticateAction->getConstructorArgumentProperty('socialProviderName')->setValue($socialProviderName);
+		$anonymousSocialAuthenticateAction->getConstructorArgumentProperty('socialProfile')->setValue($socialProfile);
+		$anonymousSocialAuthenticateAction->getConstructorArgumentProperty('hybridAuthFactory')->setValue($hybridAuthFactory);
+		$anonymousSocialAuthenticateAction->getConstructorArgumentProperty('errorMessage')->setValue($socialLoginErrorMessage);
+		$anonymousSocialAuthenticateAction->getConstructorArgumentProperty('errorException')->setValue($socialLoginException);
+		$anonymousSocialAuthenticateAction->getConstructorArgumentProperty('onSuccess')->setValue(array(0 => $anonymousPerformSocialLoginAction, ));
+		$anonymousSocialAuthenticateAction->getConstructorArgumentProperty('onFailure')->setValue(array(0 => $anonymousDisplayMessageAction, ));
+		$anonymousPerformSocialLoginAction->getConstructorArgumentProperty('socialProviderName')->setValue($socialProviderName);
+		$anonymousPerformSocialLoginAction->getConstructorArgumentProperty('socialProfile')->setValue($socialProfile);
+		$anonymousPerformSocialLoginAction->getConstructorArgumentProperty('findSocialUser')->setValue($anonymousSelect);
+		$anonymousPerformSocialLoginAction->getConstructorArgumentProperty('dbConnection')->setValue($dbConnection);
+		$anonymousPerformSocialLoginAction->getConstructorArgumentProperty('userService')->setValue($userService);
+		$anonymousPerformSocialLoginAction->getConstructorArgumentProperty('bindOnEmail')->setValue(true);
+		$anonymousPerformSocialLoginAction->getConstructorArgumentProperty('findUserIdFromMail')->setValue($anonymousSelect2);
+		$anonymousSelect->getSetterProperty('setColumns')->setValue(array(0 => $anonymousColRef, ));
+		$anonymousSelect->getSetterProperty('setFrom')->setValue(array(0 => $anonymousTable, ));
+		$anonymousSelect->getSetterProperty('setWhere')->setValue($anonymousAndOp);
+		$anonymousColRef->getSetterProperty('setColumn')->setValue('user_id');
+		$anonymousTable->getSetterProperty('setTable')->setValue('authentications');
+		$anonymousTable->getSetterProperty('setJoinType')->setValue('JOIN');
+		$anonymousAndOp->getSetterProperty('setOperands')->setValue(array(0 => $anonymousEqual, 1 => $anonymousEqual2, ));
+		$anonymousEqual->getSetterProperty('setLeftOperand')->setValue($anonymousColRef2);
+		$anonymousEqual->getSetterProperty('setRightOperand')->setValue($anonymousParameter);
+		$anonymousEqual->getSetterProperty('setCondition')->setValue($anonymousParamAvailableCondition);
+		$anonymousColRef2->getSetterProperty('setColumn')->setValue('provider');
+		$anonymousParameter->getSetterProperty('setName')->setValue('provider');
+		$anonymousParamAvailableCondition->getConstructorArgumentProperty('parameterName')->setValue('provider');
+		$anonymousEqual2->getSetterProperty('setLeftOperand')->setValue($anonymousColRef3);
+		$anonymousEqual2->getSetterProperty('setRightOperand')->setValue($anonymousParameter2);
+		$anonymousEqual2->getSetterProperty('setCondition')->setValue($anonymousParamAvailableCondition2);
+		$anonymousColRef3->getSetterProperty('setColumn')->setValue('provider_uid');
+		$anonymousParameter2->getSetterProperty('setName')->setValue('provider_uid');
+		$anonymousParamAvailableCondition2->getConstructorArgumentProperty('parameterName')->setValue('provider_uid');
+		$anonymousSelect2->getSetterProperty('setColumns')->setValue(array(0 => $anonymousColRef4, ));
+		$anonymousSelect2->getSetterProperty('setFrom')->setValue(array(0 => $anonymousTable2, ));
+		$anonymousSelect2->getSetterProperty('setWhere')->setValue($anonymousEqual3);
+		$anonymousColRef4->getSetterProperty('setColumn')->setValue('id');
+		$anonymousTable2->getSetterProperty('setTable')->setValue('users');
+		$anonymousTable2->getSetterProperty('setJoinType')->setValue('JOIN');
+		$anonymousEqual3->getSetterProperty('setLeftOperand')->setValue($anonymousColRef5);
+		$anonymousEqual3->getSetterProperty('setRightOperand')->setValue($anonymousParameter3);
+		$anonymousEqual3->getSetterProperty('setCondition')->setValue($anonymousParamAvailableCondition3);
+		$anonymousColRef5->getSetterProperty('setColumn')->setValue('email');
+		$anonymousParameter3->getSetterProperty('setName')->setValue('email');
+		$anonymousParamAvailableCondition3->getConstructorArgumentProperty('parameterName')->setValue('email');
+		$anonymousDisplayMessageAction->getConstructorArgumentProperty('message')->setValue($anonymousUserMessage);
+		$anonymousDisplayMessageAction->getConstructorArgumentProperty('messageService')->setValue($userMessageService);
+		$anonymousUserMessage->getConstructorArgumentProperty('message')->setValue($socialLoginErrorMessage);
+		$anonymousUserMessage->getConstructorArgumentProperty('type')->setValue('error');
+		
 		
 		$this->moufManager->rewriteMouf();
 		
-		TdbmController::generateDaos($this->moufManager, "tdbmService", $sourcedirectory, $daonamespace, $beannamespace, "DaoFactory", "daoFactory", $selfedit, $keepSupport);
-				
 		InstallUtils::continueInstall($selfedit == "true");
 	}
 	
@@ -125,7 +350,7 @@ class HybridAuthInstallController extends Controller {
 	
 	private function displayErrorMsg($msg) {
 		$this->errorMsg = $msg;
-		$this->content->addFile(dirname(__FILE__)."/../../../views/installError.php", $this);
+		$this->content->addFile(dirname(__FILE__)."/../../../../views/installError.php", $this);
 		$this->template->toHtml();
 	}
 		
--- src/views/installStep2.php
+++ src/views/installStep2.php
@@ -1,21 +1,149 @@
-<?php 
-<?php /* @var $this Mouf\Integration\HybridAuth\Controllers\HybridAuthInstallController */ ?>
+<?php  /* @var $this Mouf\Integration\HybridAuth\Controllers\HybridAuthInstallController */ ?>
 <h1>Setting up HybridAuth</h1>
 
 <form action="generate" method="post" class="form-horizontal">
-<input type="hidden" id="selfedit" name="selfedit" value="<?php echo plainstring_to_htmlprotected($this->selfedit) ?>" />
+	<input type="hidden" id="selfedit" name="selfedit"
+		value="<?php echo plainstring_to_htmlprotected($this->selfedit) ?>" />
 
-<div class="control-group">
-	<label class="control-label">Source directory:</label>
-	<div class="controls">
-		<input type="text" name="sourcedirectory" value="<?php echo plainstring_to_htmlprotected($this->sourceDirectory) ?>"></input>
-		<span class="help-block">This is the directory containing your source code (it should be configured in your the "autoload" section of your <em>composer.json</em> file.</span>
+	<div class="control-group">
+		<div class="controls">
+			<label class="checkbox"> <input type="checkbox" name="facebook" /> Configure Facebook Connect</label>
+		</div>
 	</div>
-</div>
 
-<div class="control-group">
-	<div class="controls">
-		<button name="action" value="generate" type="submit" class="btn btn-danger">Install HybridAuth</button>
+	<div id="facebook_group">
+		<div class="control-group">
+			<label class="control-label">Facebook id:</label>
+			<div class="controls">
+				<input type="text" name="facebook_id"
+					value="<?php //echo plainstring_to_htmlprotected($this->sourceDirectory) ?>"></input>
+				<span class="help-block">The facebook ID of your application. <a target="_blank" href="https://developers.facebook.com/apps">Don't have a Facebook app yet?</a>
+				</span>
+			</div>
+		</div>
+		
+		<div class="control-group">
+			<label class="control-label">Facebook secret:</label>
+			<div class="controls">
+				<input type="text" name="facebook_secret"
+					value="<?php //echo plainstring_to_htmlprotected($this->sourceDirectory) ?>"></input>
+				<span class="help-block">The facebook secret of your application.
+				</span>
+			</div>
+		</div>
+		
+		<div class="control-group">
+			<label class="control-label">Scope (optional):</label>
+			<div class="controls">
+				<input type="text" name="facebook_scope"
+					value="<?php //echo plainstring_to_htmlprotected($this->sourceDirectory) ?>"></input>
+				<span class="help-block">The scope requested by your application, separated by commas.
+				For instance: "email, user_about_me, user_birthday, user_hometown"
+				</span>
+			</div>
+		</div>	
 	</div>
-</div>
+	
+	
+	
+	
+	<div class="control-group">
+		<div class="controls">
+			<label class="checkbox"> <input type="checkbox" name="twitter" /> Configure Twitter provider</label>
+		</div>
+	</div>
+
+	<div id="twitter_group">
+		<div class="control-group">
+			<label class="control-label">Twitter Key:</label>
+			<div class="controls">
+				<input type="text" name="twitter_key"
+					value="<?php //echo plainstring_to_htmlprotected($this->sourceDirectory) ?>"></input>
+				<span class="help-block">The Twitter key of your application. <a target="_blank" href="https://dev.twitter.com/apps">Don't have a Twitter app yet?</a>
+				</span>
+			</div>
+		</div>
+		
+		<div class="control-group">
+			<label class="control-label">Twitter secret:</label>
+			<div class="controls">
+				<input type="text" name="twitter_secret"
+					value="<?php //echo plainstring_to_htmlprotected($this->sourceDirectory) ?>"></input>
+				<span class="help-block">The Twitter secret of your application.
+				</span>
+			</div>
+		</div>
+		
+	</div>
+	
+	
+	<div class="control-group">
+		<div class="controls">
+			<label class="checkbox"> <input type="checkbox" name="google" /> Configure Google provider</label>
+		</div>
+	</div>
+
+	<div id="google_group">
+		<div class="control-group">
+			<label class="control-label">Google ID:</label>
+			<div class="controls">
+				<input type="text" name="google_id"
+					value="<?php //echo plainstring_to_htmlprotected($this->sourceDirectory) ?>"></input>
+				<span class="help-block">The Google ID of your application. <a target="_blank" href="https://code.google.com/apis/console/">Don't have a Google app yet?</a>
+				</span>
+			</div>
+		</div>
+		
+		<div class="control-group">
+			<label class="control-label">Google secret:</label>
+			<div class="controls">
+				<input type="text" name="google_secret"
+					value="<?php //echo plainstring_to_htmlprotected($this->sourceDirectory) ?>"></input>
+				<span class="help-block">The Google secret of your application.
+				</span>
+			</div>
+		</div>
+		
+	</div>
+	
+	<div class="control-group">
+		<div class="controls">
+			<button name="action" value="generate" type="submit"
+				class="btn btn-danger">Install HybridAuth</button>
+		</div>
+	</div>
 </form>
+
+<script type="text/javascript">
+function updateFacebook() {
+	if ($("input[name=facebook]").prop('checked')) {
+		$("#facebook_group").show();
+	} else {
+		$("#facebook_group").hide();
+	}
+}
+$("input[name=facebook]").change(updateFacebook);
+updateFacebook();
+
+function updateGoogle() {
+	if ($("input[name=google]").prop('checked')) {
+		$("#google_group").show();
+	} else {
+		$("#google_group").hide();
+	}
+}
+$("input[name=google]").change(updateGoogle);
+updateGoogle();
+
+
+function updateTwitter() {
+	if ($("input[name=twitter]").prop('checked')) {
+		$("#twitter_group").show();
+	} else {
+		$("#twitter_group").hide();
+	}
+}
+$("input[name=twitter]").change(updateTwitter);
+updateTwitter();
+
+</script>
