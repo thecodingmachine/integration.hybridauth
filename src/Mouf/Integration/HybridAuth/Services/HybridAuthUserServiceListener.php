0f80573dd97dfbb917396c6741a779d5d18b4440
Marc TEYSSIER <m.teyssier@thecodingmachine.com>
2016-12-12 07:48:53 +0100

    Fix package use for database.patcher Add class for authenticateListener

---
+++ src/Mouf/Integration/HybridAuth/Services/HybridAuthUserServiceListener.php
@@ -0,0 +1,35 @@
+<?php
+namespace Mouf\Integration\HybridAuth\Services;
+use Mouf\Security\UserService\AuthenticationListenerInterface;
+use Mouf\Security\UserService\UserServiceInterface;
+use Mouf\Integration\HybridAuth\HybridAuthFactory;
+class HybridAuthUserServiceListener implements AuthenticationListenerInterface {
+	/**
+	 * 
+	 * @var HybridAuthFactory
+	 */
+	private $hybridAuthFactory;
+	
+	public function __construct(HybridAuthFactory $hybridAuthFactory) {
+		$this->hybridAuthFactory = $hybridAuthFactory;
+	}
+	
+	/**
+	 * This method is called just after a log-in occurs.
+	 *
+	 * @param UserServiceInterface $userService The service that performed the log-in
+	 */
+	public function afterLogIn(UserServiceInterface $userService) {
+		
+	}
+	
+	/**
+	 * This method is called just before the current user logs out.
+	 *
+	 * @param UserServiceInterface $userService The service that performed the log-out
+	*/
+	public function beforeLogOut(UserServiceInterface $userService) {
+		$hybridAuth = $this->hybridAuthFactory->getHybridAuth();
+		$hybridAuth->logoutAllProviders();
+	}
+}
